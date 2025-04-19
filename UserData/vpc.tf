56d6564a9f61e9bb5bf7dad1b3d93b1f72b406e1
Luiz Fernando Povoa <luizfer1@yahoo.com.br>
2025-04-19 18:39:25 -0300

    Added SSH permission

---
+++ UserData/vpc.tf
@@ -0,0 +1,11 @@
+#Create AWS VPC
+resource "aws_vpc" "levelup_vpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+  enable_dns_support = "true"
+  enable_dns_hostnames = "true"
+
+  tags = {
+    Name = "levelup_vpc"
+  }
+}
