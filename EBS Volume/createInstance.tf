87ceef58a6521988fc156239052af236a83ed83d
Luiz Fernando Povoa <luizfer1@yahoo.com.br>
2025-04-18 21:00:11 -0300

    EBS Volume

--- EBS Volume/createInstance.tf
+++ EBS Volume/createInstance.tf
@@ -17,7 +17,7 @@ resource "aws_instance" "MyFirstInstnace" {
 
 #EBS resource Creation
 resource "aws_ebs_volume" "ebs-volume-1" {
-  availability_zone = "us-east-2a"
+  availability_zone = "us-east-2b"
   size              = 50
   type              = "gp2"
 
